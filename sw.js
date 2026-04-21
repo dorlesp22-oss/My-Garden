9100c940d40c2197373ace7aedf5c3e80bfae22c
dorlesp22-oss <dorle.sp22@gmail.com>
2026-04-21 19:13:00 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,8 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c))))); self.clients.claim(); });
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request.url + '?v=' + Date.now()).catch(() => fetch(e.request)));
+});
